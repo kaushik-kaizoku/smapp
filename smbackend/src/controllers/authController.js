e937362729f6e76f5b977cb76a652d6187ca7cc2
KAUSHIK <kaushikhrushikesh514@gmail.com>
2025-01-18 01:00:24 +0530

    corrected jwt

--- smbackend/src/controllers/authController.js
+++ smbackend/src/controllers/authController.js
@@ -16,7 +16,7 @@ const registerAdmin = async(req,res) => {
         }    
       })
       const token = jwt.sign(
-        { id: user.id, role: user.role },
+        { id: user.id },
         process.env.JWT_SECRET,
         { expiresIn: '1d' }
       );
@@ -41,7 +41,7 @@ const login = async(req,res) => {
         return res.status(401).json({ error: 'Invalid credentials' });
   
       const token = jwt.sign(
-        { id: user.id, role: user.role },
+        { id: user.id },
         process.env.JWT_SECRET,
         { expiresIn: '1d' }
       );
--- smfrontend/src/config.ts
+++ smfrontend/src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://127.0.0.1:3000"
+export const BACKEND_URL = "https://smapp-utln.onrender.com"
